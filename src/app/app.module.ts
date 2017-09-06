07b0282b6508b7f57b1a651945e6720b3cf0e9a6
Zvukamar <fre3run@gmail.com>
2017-09-06 17:38:16 +0200

    Added login, page-not-found-register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
+
+/* import Modules */
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* import Components */
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+    },
+    {
+      path: 'login',
+      component: LoginFormComponent
+    },
+    {
+      path: 'registration',
+      component: RegistrationComponent
+    },
+    {
+      path: '**',
+      component: PageNotFoundComponent
+    }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegistrationComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
