bebbb2754155c16708fb97a174c5b95bdfdad3f2
mazmaz2k <mazmaz2k@gmail.com>
2017-09-18 22:09:39 +0300

    hi

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { element } from 'protractor';
 import { ToDoListComponent } from './../to-do-list/to-do-list.component';
@@ -14,26 +15,22 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export class MyProfileComponent implements OnInit {
   form: FormGroup;
-// public items: FirebaseListObservable<any[]>;
   private _user;
   @ViewChild('firstName') firstInput: ElementRef;
   @ViewChild('lastName') lastInput: ElementRef;
- // @ViewChild('email') emailInput: ElementRef;
 
-  constructor( private list: ToDoListComponent, private userServ: UsersService,  private logreg: LogRegService, 
+  constructor( private list: ToDoListComponent, private userServ: UsersService, private router: Router , private logreg: LogRegService, 
     private users: UsersService ) {
-     this.userServ.getUserData(this.logreg.userUID);
+     this.users.getUserData(this.logreg.userUID);
     this._user = list.user;
     this.userServ.userData.subscribe(user => {
       this._user = user;
-       this.firstInput.nativeElement.value = this._user.firstName;
-       this.lastInput.nativeElement.value = this._user.lastName;
-     //  this.emailInput.nativeElement.value = this._user.email;
+      // this.firstInput.nativeElement.value = this._user.firstName;
+      // this.lastInput.nativeElement.value = this._user.lastName;
       });
       this.form = new FormGroup({
         firstName: new FormControl('', Validators.required),
         lastName: new FormControl('', Validators.required),
-       // email: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\(.[A-Za-z]{1,63}$)+')]),
       });
   }
 
@@ -47,6 +44,7 @@ export class MyProfileComponent implements OnInit {
     this._user.lastName = this.form.controls['lastName'].value;
     // this._user.email = this.form.controls['email'].value;
     this.users.setUserData(this._user, this.logreg.userUID);
+    this.router.navigate(['list']);
   }
   checkValid(name) {
     return (this.form.controls[name].invalid && this.form.controls[name].touched);
