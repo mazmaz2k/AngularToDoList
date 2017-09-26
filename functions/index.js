39ceb1279e5e4f809aac98d6cbdc9d5461f0d3ee
Zvukamar <fre3run@gmail.com>
2017-09-26 23:28:50 +0300

    added multi notification

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ exports.fcmSend = functions.database.ref('/items/{userUID}').onWrite(event => {
     const post = event.data.val();
     const payload = {
           notification: {
-            title: 'OMRI IS VERY VERY VERY VERY GAY',
+            title: ' TITLE IS : OMRI IS VERY VERY VERY VERY VERY VERY VERY GAY',
             body: 'OMRI IS VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY GAY',
             icon: "https://placeimg.com/250/250/people"
           }
@@ -16,20 +16,35 @@ exports.fcmSend = functions.database.ref('/items/{userUID}').onWrite(event => {
           .ref(`/fcmTokens/${userId}`).once("value" , function(snapshot) {
             token = snapshot.val().myToken;
             return admin.messaging().sendToDevice(token, payload);
-          })
-          .then(res => {
-            console.log("Sent Successfully", res);
-          })
-          .catch(err => {
-            console.log(err);
           });
+          // .then(res => {
+          //   console.log("Sent Successfully", res);
+          // })
+          // .catch(err => {
+          //   console.log(err);
+          // });
           getTime(userId);
 });
 
 function getTime(userId) {
-  console.log('in get time');
+  const payload = {
+    notification: {
+      title: 'Reminding',
+      body: 'kasdjkdsf',
+      icon: "https://placeimg.com/250/250/people"
+    }
+  };  
+  var now = new Date().getTime();
+  console.log('today second is ', now);
   admin.database().ref(`/items/${userId}`).once("value", function(snapshot) {
     console.log('this is snapshot');
     console.log(snapshot.val());
+    for ( var i in snapshot.val() )
+    {
+        if (snapshot.val()[i].toSec - now < 60000000000) { // 60sec * 10min = 600 sec 
+          console.log('notification');
+          return admin.messaging().sendToDevice(token, payload);
+        } 
+    }
   });
 }
